16c873ee9e5308a14ff549657bd5e4116214d0aa
root <root@localhost.localdomain>
2018-09-25 14:39:05 -0400

    ip renew

--- poc.py
+++ poc.py
@@ -1,13 +1,28 @@
 import requests
+import time
+from stem import Signal
+from stem.control import Controller
+
 """
 SocksPort 9050 # Default: Bind to localhost:9050 for local connections.
 SocksPort 127.0.0.1:9100 # Bind to this address:port too.
 """
 def get_tor_session():
-    session = requests.session()
-    # Tor uses the 9050 port as the default socks port
-    session.proxies = {'http':  'socks5://127.0.0.1:9050','https': 'socks5://127.0.0.1:9050'}
-    return session
+	session = requests.session()
+	# Tor uses the 9050 port as the default socks port
+	session.proxies = {'http':  'socks5://127.0.0.1:9050','https': 'socks5://127.0.0.1:9050'}
+	return session
+
+def renewIP():
+	print 'entre a renew ip'
+	try:
+		with Controller.from_port(port = 9051) as controller:
+			controller.authenticate()
+			controller.signal(Signal.NEWNYM)
+	except Exception as e:
+		print 'Oie no '
+		print e
+	print 'sali de renew ip'
 
 # Make a request through the Tor connection
 # IP visible through Tor
@@ -17,3 +32,9 @@ print(session.get("http://httpbin.org/ip").text)
 
 # Following prints your normal public IP
 print(requests.get("http://httpbin.org/ip").text)
+
+
+renewIP()
+session = get_tor_session()
+time.sleep(4)
+print(session.get("http://httpbin.org/ip").text)
